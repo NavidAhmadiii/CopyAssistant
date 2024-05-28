d59d18b8c5e28b962b71e6b8b2aede2832bb4bd7
Navid <navidahmadi6293@gmail.com>
2024-05-28 11:39:10 +0330

    add new database

--- config/settings.py
+++ config/settings.py
@@ -60,10 +60,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CopyAssistant',
+        'USER': 'postgres',
+        'PASSWORD': 'navid0832',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
