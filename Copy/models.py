bc644ff8ee0dccfe99394d7e86fd26a643a1b91a
Navid <navidahmadi6293@gmail.com>
2024-05-28 11:50:21 +0330

    add models

--- Copy/models.py
+++ Copy/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class PDFDocument(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='pdfs/')
+
+
+class UserEntry(models.Model):
+    pdf = models.ForeignKey(PDFDocument, on_delete=models.CASCADE)
+    user_text = models.TextField()
+    spell_checked_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Copy.urls')),
+    # path('', include('Copy.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
